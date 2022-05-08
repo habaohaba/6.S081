7e7557702149f74c987826cb7dabc6b2dfcf2043
habaohaba <15313137931lz@gmail.com>
2022-05-08 21:34:28 +0800

    finish pte printout

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,36 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint_helper2(int i){
+    //show depth
+    for(int j = 0; j <= i; j++){
+        printf("..");
+    }
+}
+void vmprint_helper(pagetable_t pagetable, int level){
+     // there are 2^9 = 512 PTEs in a page table.
+    for(int i = 0; i < 512; i++){
+        pte_t pte = pagetable[i];
+        if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+            // this PTE points to a lower-level page table.
+            if(level == 2)
+                panic("third page table still point to lower level pgtbl");
+            uint64 child = PTE2PA(pte);
+            vmprint_helper2(level);        
+            printf("%d: pte %p pa %p\n", i, pte, child);
+            vmprint_helper((pagetable_t) child, level+1);
+        } else if(pte & PTE_V){
+            //leaf page
+            uint64 pa = PTE2PA(pte);
+            vmprint_helper2(level);        
+            printf("%d: pte %p pa %p\n", i, pte, pa);
+        }
+    }
+}
+void
+vmprint(pagetable_t pagetable){
+    printf("page table %p\n", pagetable);
+    vmprint_helper(pagetable, 0);
+}
+
